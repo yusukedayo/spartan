d0cd808573f740cfc0234c31be2431abfe3bb3a7
yusuke <tsunemi0217@icloud.com>
2021-10-09 15:26:50 +0900

    修正版です。
    ・インデントの変更
    ・任意の数よりも１少ない数までしか表示されなかった（30を入力したら29までの結果しか表示されない）ので修正しました。

--- practice.fizzbuzz.rb
+++ practice.fizzbuzz.rb
@@ -1,14 +1,14 @@
-number = ARGV[0].to_i
+number = ARGV[0].to_i + 1
 p number
-    number.times do |x|
-        number += 1
-        if x % 15 == 0
-            puts "FizzBuzz"
-        elsif x % 5 == 0
-            puts "Buzz"
-        elsif x % 3 == 0
-            puts "Fizz"
-        else
-            puts x
-        end
-    end
+number.times do |x|
+  number += 1
+  if x % 15 == 0
+    puts "FizzBuzz!"
+  elsif x % 5 == 0
+    puts "Buzz!"
+  elsif x % 3 == 0
+    puts "Fizz!"
+  else
+    puts x
+  end
+end
